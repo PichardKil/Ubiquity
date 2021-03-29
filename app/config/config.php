028ef035fe0388396625d43ed5681992ead3645a
Kilian <kil.pichard@icloud.com>
2021-03-29 13:54:26 +0200

    Connection to DB

--- app/config/config.php
+++ app/config/config.php
@@ -1,29 +1,48 @@
 <?php
 return array(
-		"siteUrl"=>"http://127.0.0.1/store/",
-		"database"=>[
-				"type"=>"mysql",
-				"dbName"=>"",
-				"serverName"=>"127.0.0.1",
-				"port"=>"3306",
-				"user"=>"root",
-				"password"=>"",
-				"options"=>[],
-				"cache"=>false
-		],
-		"sessionName"=>"Store",
-		"namespaces"=>[],
-		"templateEngine"=>'Ubiquity\\views\\engine\\Twig',
-		"templateEngineOptions"=>array("cache"=>false),
-		"test"=>false,
-		"debug"=>false,
-		"logger"=>function(){return new \Ubiquity\log\libraries\UMonolog("test",\Monolog\Logger::INFO);},
-		"di"=>["@exec"=>["jquery"=>function($controller){
+	"siteUrl"=>"http://127.0.0.1:8090/",
+	"database"=>[
+			"type"=>"mysql",
+			"dbName"=>"store",
+			"serverName"=>"127.0.0.1",
+			"port"=>8889,
+			"user"=>"root",
+			"password"=>"root",
+			"options"=>array(),
+			"cache"=>false,
+			"wrapper"=>"Ubiquity\\db\\providers\\pdo\\PDOWrapper"
+			],
+	"sessionName"=>"s6061af1bf025a",
+	"namespaces"=>array(),
+	"templateEngine"=>"Ubiquity\\views\\engine\\Twig",
+	"templateEngineOptions"=>[
+			"cache"=>false
+			],
+	"test"=>false,
+	"debug"=>true,
+	"logger"=>function (){
+		return new \Ubiquity\log\libraries\UMonolog(array (
+  'host' => '127.0.0.1',
+  'port' => 8090,
+  'sessionName' => 's6061af1bf025a',
+)['sessionName'], \Monolog\Logger::INFO);
+	},
+	"di"=>[
+			"@exec"=>array("jquery"=>function ($controller){
 						return \Ubiquity\core\Framework::diSemantic($controller);
-					}]],
-		"cache"=>["directory"=>"cache/","system"=>"Ubiquity\\cache\\system\\ArrayCache","params"=>[]],
-		"mvcNS"=>["models"=>"models","controllers"=>"controllers","rest"=>""],
-		"isRest"=>function(){
+					})
+			],
+	"cache"=>[
+			"directory"=>"cache/",
+			"system"=>"Ubiquity\\cache\\system\\ArrayCache",
+			"params"=>array()
+			],
+	"mvcNS"=>[
+			"models"=>"models",
+			"controllers"=>"controllers",
+			"rest"=>""
+			],
+	"isRest"=>function (){
 			return \Ubiquity\utils\http\URequest::getUrlParts()[0]==="rest";
 		}
-);
+	);
